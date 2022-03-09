0daf53bb3ca2675d7259493dccdd33f68e472314
Olexantor <nikolaev.warrior@gmail.com>
2022-03-09 18:19:26 +0500

    Currency VC
    setup currency VC, add currency cell

---
+++ Exchange/CurrencyCell.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyCell.swift
+//  Exchange
+//
+//  Created by Александр on 09.03.2022.
+//
+
+import UIKit
+
+final class CurrencyCell: UITableViewCell {
+    static let identifier = "currency cell"
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
