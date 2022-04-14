8e3533f0de6daa36ce38ba01f802a65345775aa7
Александр Николаев <nikolaev.warrior@gmail.com>
2022-04-14 19:43:47 +0500

    ExchangeViewModel
    Refactoring code with Rx Swift

--- Exchange/ExchangeView/ExchangeViewModel.swift
+++ Exchange/ExchangeView/ExchangeViewModel.swift
@@ -38,39 +38,13 @@ extension ExchangeViewModel: ViewModelType {
     ) -> Self {
         
         let didReceiveError = PublishRelay<String>()
-        let firstCurrency = BehaviorRelay(value: "")
-        let secondCurrency = BehaviorRelay(value: "")
-        let ratesForFirstCurrency = BehaviorRelay(value: [String:Double]())
-        let ratesForSecondCurrency = BehaviorRelay(value: [String:Double]())
-        let textOfFirstCurrencyTextField = BehaviorRelay(value: "")
-        let textOfSecondCurrencyTextField =  BehaviorRelay(value: "")
         
-        let firstButtonTapDisposable = binding.didTapFirstCurrencySelectionButton
-            .flatMap { _ in
-                router.showSelectCurrencyViewController()
-            }
-            .emit(to: firstCurrency)
-        
-        let firstCurrencyRatesDisposable = firstCurrency
-            .skip(1)
-            .flatMapLatest { currency -> Single<ExchangeRate> in
-                dependency
-                    .networkService
-                    .fetchExchangeRate(for: currency)
-            }
-            .map { $0.rates }
-            .asDriver { error in
-                didReceiveError.accept(error.localizedDescription)
-                return .empty()
-            }
-            .drive(ratesForFirstCurrency)
-        
-        let secondButtonTapDisposable = binding.didTapSecondCurrencySelectionButton
+        let firstButtonTap = binding.didTapFirstCurrencySelectionButton
             .flatMap(router.showSelectCurrencyViewController)
-            .emit(to: secondCurrency)
+            .asDriver(onErrorJustReturn: "")
         
-        let secondCurrencyRatesDisposable = secondCurrency
-            .skip(1)
+        let firstCurrencyRates = firstButtonTap
+            .asObservable()
             .flatMapLatest { currency -> Single<ExchangeRate> in
                 dependency
                     .networkService
@@ -81,63 +55,68 @@ extension ExchangeViewModel: ViewModelType {
                 didReceiveError.accept(error.localizedDescription)
                 return .empty()
             }
-            .drive(ratesForSecondCurrency)
         
-        let firstTextFieldDisposable = binding.textOfFirstCurrencyTextField
-            .skip(1)
-            .compactMap { $0 }
-            .map {
-                guard !firstCurrency.value.isEmpty && !secondCurrency.value.isEmpty else {
-                    return Double()
-                }
-                guard let value = Double($0) else { return Double() }
-                return value
-            }
-            .map {
-                String(
-                    format: "%0.2f",
-                    ($0 * Double(ratesForFirstCurrency.value[secondCurrency.value] ?? 0.0))
-                )
-            }
-            .drive(textOfSecondCurrencyTextField)
+        let secondButtonTap = binding.didTapSecondCurrencySelectionButton
+            .flatMap(router.showSelectCurrencyViewController)
+            .asDriver(onErrorJustReturn: "")
         
-        let secondTextFieldDisposable = binding.textOfSecondCurrencyTextField
-            .skip(1)
-            .compactMap { $0 }
-            .map {
-                guard !firstCurrency.value.isEmpty && !secondCurrency.value.isEmpty else {
-                    return Double()
-                }
-                guard let value = Double($0) else { return Double() }
-                return value
+        let secondCurrencyRates = secondButtonTap
+            .asObservable()
+            .flatMapLatest { currency -> Single<ExchangeRate> in
+                dependency
+                    .networkService
+                    .fetchExchangeRate(for: currency)
             }
-            .map {
-                String(
-                    format: "%0.2f",
-                    ($0 * Double(ratesForSecondCurrency.value[firstCurrency.value] ?? 0.0))
-                )
+            .map { $0.rates }
+            .asDriver { error in
+                didReceiveError.accept(error.localizedDescription)
+                return .empty()
             }
-            .drive(textOfFirstCurrencyTextField)
+        
+        let firstCurrencyCalculatedValue = Driver.combineLatest(
+            firstButtonTap,
+            binding.textOfSecondCurrencyTextField,
+            firstCurrencyRates,
+            secondCurrencyRates
+        ) {
+            firstCurrency, text, ratesOfFirstCurrency, ratesOfSecondCurrency -> String in
+            guard !ratesOfFirstCurrency.isEmpty && !ratesOfSecondCurrency.isEmpty else {
+                return String()
+            }
+            guard let text = text, let number = Double(text) else { return String() }
+            return String(
+                format: "%0.2f",
+                (number * Double(ratesOfSecondCurrency[firstCurrency] ?? 0.0))
+            )
+        }
+        
+        let secondCurrencyCalculatedValue = Driver.combineLatest(
+            secondButtonTap,
+            binding.textOfFirstCurrencyTextField,
+            firstCurrencyRates,
+            secondCurrencyRates
+        ) {
+            secondCurrency, text, ratesOfFirstCurrency, ratesOfSecondCurrency -> String in
+            guard !ratesOfFirstCurrency.isEmpty && !ratesOfSecondCurrency.isEmpty else {
+                return String()
+            }
+            guard let text = text, let number = Double(text) else { return String() }
+            return String(
+                format: "%0.2f",
+                (number * Double(ratesOfFirstCurrency[secondCurrency] ?? 0.0))
+            )
+        }
         
         let showErrorDisposable = didReceiveError
             .asSignal()
             .emit(onNext: router.showAlert)
         
-        let disposables = CompositeDisposable(
-            firstButtonTapDisposable,
-            firstCurrencyRatesDisposable,
-            secondButtonTapDisposable,
-            secondCurrencyRatesDisposable,
-            firstTextFieldDisposable,
-            secondTextFieldDisposable,
-            showErrorDisposable
-        )
-        
         return .init(
-            firstCurrency: firstCurrency.asDriver(),
-            secondCurrency: secondCurrency.asDriver(),
-            firstCurrencyCalculatedValue: textOfFirstCurrencyTextField.asDriver(),
-            secondCurrencyCalculatedValue: textOfSecondCurrencyTextField.asDriver(),
-            disposables: disposables)
+            firstCurrency: firstButtonTap,
+            secondCurrency: secondButtonTap,
+            firstCurrencyCalculatedValue: firstCurrencyCalculatedValue,
+            secondCurrencyCalculatedValue: secondCurrencyCalculatedValue,
+            disposables: showErrorDisposable
+        )
     }
 }
