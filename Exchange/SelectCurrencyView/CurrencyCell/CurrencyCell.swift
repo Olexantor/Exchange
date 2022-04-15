435366555e0a0a87d8ef1fd6f97fd0c32fc22fa0
Александр Николаев <nikolaev.warrior@gmail.com>
2022-04-15 17:15:38 +0500

    CurrencyCell
    update layout with pin layout

--- Exchange/SelectCurrencyView/CurrencyCell/CurrencyCell.swift
+++ Exchange/SelectCurrencyView/CurrencyCell/CurrencyCell.swift
@@ -5,41 +5,48 @@
 //  Created by Александр on 09.03.2022.
 //
 
+import PinLayout
 import UIKit
 
 final class CurrencyCell: UITableViewCell {
     static let identifier = "currency cell"
+    
     weak var viewModel: CurrencyCellViewModelType? {
         didSet {
             guard let viewModel = viewModel else { return }
-            currencyLabel.text = viewModel.currency
+            ui.currencyLabel.text = viewModel.currency
         }
     }
     
-    private let currencyLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 24)
-        return label
-    }()
+    private lazy var ui = createUI()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        addSubview(currencyLabel)
-        setupConstrains()
+        layoutUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setupConstrains() {
-        currencyLabel.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
-            make.top.equalToSuperview().offset(8)
-            make.bottom.equalToSuperview().offset(-8)
-        }
-    }
 }
 
+private extension CurrencyCell {
+    
+    struct UI {
+        let currencyLabel: UILabel
+    }
+    
+    func createUI() -> UI {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        contentView.addSubview(label)
+        contentView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+
+        return .init(currencyLabel: label)
+    }
+    
+    func layoutUI() {
+        ui.currencyLabel.pin.all().vertically(8)
+    }
+}
